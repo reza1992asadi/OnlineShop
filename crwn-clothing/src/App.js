4b6bbf664f3c618232fee0cca690bbf154c4e88f
reza <reza.asadi.3363@gmail.com>
2024-10-25 03:56:03 +0200

    react router started here

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,34 +1,12 @@
+import {Route, Routes} from 'react-router-dom';
+import Home from './routes/home/home.component'; 
 
-import CategoryMenu from './components/category-menu/category-menu.component';
-
-export default function App() {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
-  return <CategoryMenu categories={categories} />; 
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element= {<Home />} />
+    </Routes>
+);
 };
+
+export default App; 
