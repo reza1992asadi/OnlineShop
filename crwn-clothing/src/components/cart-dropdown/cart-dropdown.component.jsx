74e26caa5f21bd492b63eac52babe83e4b64ca28
reza <reza.asadi.3363@gmail.com>
2025-02-14 18:06:36 +0100

    shopping list drop down is set, and can be closed and opened

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
+import { cartContext } from '../../contexts/cart.context';
 import './cart-dropdown.styles.scss' 
 import Button from '../button/button.component'; 
 
 const CartDropdown =() => {
+    const {cartOpen} = useContext(cartContext) ; 
+    if (cartOpen === true) {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -9,7 +13,7 @@ const CartDropdown =() => {
             </div>
             <Button>Checkout</Button>
         </div>
-    );
+    );}
 };
 
 export default CartDropdown ;
--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
+import { cartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss'; 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 const CartIcon = () => {
+    const {cartOpen, setCartOpen} = useContext(cartContext); 
     return (
-        <div className='.cart-icon-container'>
+        <div className='cart-icon-container' onClick={() => setCartOpen(!cartOpen)}>
             <ShoppingIcon className='shopping-icon' />
             <span className='item-count'>10</span>
         </div>
